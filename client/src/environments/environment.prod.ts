1cc9808501ee37c8b8a0e4b46fae47ae6847c00d
sandesh <sandesh.sawant@sparklesoft.co.in>
2025-11-04 15:48:49 +0530

    modified production url.

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://productmanagementbackend.vercel.app/' // Change this to your actual production API URL
+    apiUrl: 'https://productmanagementbackend.vercel.app' // Change this to your actual production API URL
 };
